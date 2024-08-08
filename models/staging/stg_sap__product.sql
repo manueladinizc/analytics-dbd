2a77fe970abc2a2197b209d7b92fe8f5f31c50dc
manueladinic <manuela.mdc@gmail.com>
2024-08-08 19:06:17 -0300

    create stg product table

---
+++ models/staging/stg_sap__product.sql
@@ -0,0 +1,10 @@
+with
+    product as (
+        select
+           {{ int_col('productid') }} as product_id
+            , {{ string_col('name') }} as product_name
+            , {{ string_col('productnumber') }} as product_number
+        from {{ source('sap_adw', 'product') }}
+    )
+select *
+from product
