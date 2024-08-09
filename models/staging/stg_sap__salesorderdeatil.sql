236bce517774c3b3cb16bc318177694e94a696df
manueladinic <manuela.mdc@gmail.com>
2024-08-08 22:05:15 -0300

    create stg salesorderdetail and salesreason

---
+++ models/staging/stg_sap__salesorderdeatil.sql
@@ -0,0 +1,14 @@
+with
+    salesorderdeatil as (
+        select
+            {{ int_col('salesorderid') }} as sales_order_id
+            , {{ int_col('salesorderdetailid') }} as sales_order_detail_id
+            , {{ int_col('productid') }} as product_id
+            , {{ int_col('orderqty') }} as order_qty
+            , cast(unitprice as numeric) as unit_price
+            , cast(unitpricediscount as numeric) as unit_price_discount
+            , {{ int_col('specialofferid') }} as special_offer_id
+        from {{ source('sap_adw', 'salesorderdetail') }}
+    )
+select *
+from salesorderdeatil
