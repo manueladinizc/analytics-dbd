f05fb7558eaa7e57d60558e9d1908a3028d8ad90
manueladinic <manuela.mdc@gmail.com>
2024-08-08 21:26:33 -0300

    create and update stg address stateprovince countryregion

---
+++ models/staging/stg_sap__address.sql
@@ -0,0 +1,11 @@
+with
+    address as (
+        select
+            {{ int_col('addressid') }} as address_id
+            , {{ int_col('stateprovinceid') }} as state_province_id
+            , {{ string_col('city') }} as city
+        from {{ source('sap_adw', 'address') }}
+    )
+
+select *
+from address
--- models/staging/stg_sap__countryregion.sql
+++ models/staging/stg_sap__countryregion.sql
@@ -2,7 +2,7 @@ with
     countryregion as (
         select 
             {{ string_col('countryregioncode') }} as country_region_code
-            , {{ string_col('name') }} as country_name
+            , {{ string_col('name') }} as country_region_name
         from {{ source('sap_adw', 'countryregion') }}
     )
 select *
