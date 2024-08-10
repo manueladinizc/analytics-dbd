1f4f5a47b7a1f12780f45ba19e33211cf539c086
manueladinic <manuela.mdc@gmail.com>
2024-08-10 18:20:27 -0300

    create stg salesperson table and adjust code errors

--- models/staging/stg_sap__salesorderheader.sql
+++ models/staging/stg_sap__salesorderheader.sql
@@ -4,15 +4,15 @@ with
             {{ int_col('salesorderid') }} as sales_order_id
             , {{ int_col('customerid') }} as customer_id
             , {{ int_col('salespersonid') }} as sales_person_id
-            , coalesce({{ int_col('creditcardid') }}, ) as credit_card_id
+            , {{ int_col('creditcardid') }} as credit_card_id
             , {{ int_col('territoryid') }} as territory_id
             , {{ int_col('shiptoaddressid') }} as ship_to_address_id
             , {{ int_col('status') }} as status
-            , cast('subtotal', 'numeric') as sub_total
-            , cast('taxamt', 'numeric') as tax_amount
-            , cast('freight', 'numeric') as freight
-            , cast('totaldue', 'numeric') as total_due
-            , cast(orderdate as date) as order_date
+            , cast(subtotal as numeric) as sub_total
+            , cast(taxamt as numeric) as tax_amount
+            , cast(freight as numeric) as freight
+            , cast(totaldue as numeric) as total_due
+            , date(orderdate) as order_date
         from {{ source('sap_adw', 'salesorderheader') }}
     )
 select *
