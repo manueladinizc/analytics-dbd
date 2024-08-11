6f81b6c2cbf71f465530c5f54d3d154b98b60629
manueladinic <manuela.mdc@gmail.com>
2024-08-11 20:13:37 -0300

    create dim_salesreason

--- models/staging/stg_sap__salesorderheadersalesreason.sql
+++ models/staging/stg_sap__salesorderheadersalesreason.sql
@@ -2,7 +2,7 @@ with
     salesreason as (
         select
             {{ int_col('salesreasonid') }} as sales_reason_id
-            , {{ string_col('name') }} as sales_reason_description
+            , coalesce({{ string_col('name') }}, 'not provided') as sales_reason_description
             , coalesce({{ string_col('reasontype') }}, 'not provided') as reason_type
         from {{ source('sap_adw', 'salesreason') }}
     ),
