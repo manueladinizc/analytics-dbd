a2df10913ecd82bb3e637cd0c2eacceb0ab3c014
manueladinic <manuela.mdc@gmail.com>
2024-08-11 15:12:50 -0300

    split productcategory in category and subcategory to improve granularity

--- models/staging/stg_sap__productcategory.sql
+++ models/staging/stg_sap__productcategory.sql
@@ -4,26 +4,6 @@ with
             {{ int_col('productcategoryid') }} as product_category_id,
             {{ string_col('name') }} as product_category_name
         from {{ source('sap_adw', 'productcategory') }}
-    ),
-
-    productsubcategory as (
-        select
-            {{ int_col('productsubcategoryid') }} as product_subcategory_id,
-            {{ int_col('productcategoryid') }} as product_category_id,
-            {{ string_col('name') }} as product_subcategory_name
-        from {{ source('sap_adw', 'productsubcategory') }}
-    ),
-
-    join_category_subcategory as (
-        select
-            ps.product_subcategory_id
-            , ps.product_subcategory_name
-            , pc.product_category_id
-            , pc.product_category_name
-        from productsubcategory ps
-        inner join productcategory pc
-            on ps.product_category_id = pc.product_category_id
     )
-
 select *
-from join_category_subcategory
+from productcategory
