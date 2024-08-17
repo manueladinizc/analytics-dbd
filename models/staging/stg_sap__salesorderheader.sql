2f4f3cc9287f6d062f1f73ad360fef176c9515ce
manueladinizc <manuela.mdc@gmail.com>
2024-08-17 15:30:28 -0300

    change status column specifications

--- models/staging/stg_sap__salesorderheader.sql
+++ models/staging/stg_sap__salesorderheader.sql
@@ -7,12 +7,20 @@ with
             , {{ int_col('creditcardid') }} as credit_card_id
             , {{ int_col('territoryid') }} as territory_id
             , {{ int_col('shiptoaddressid') }} as ship_to_address_id
-            , {{ int_col('status') }} as status
             , cast(subtotal as numeric) as sub_total
             , cast(taxamt as numeric) as tax_amount
             , cast(freight as numeric) as freight
             , cast(totaldue as numeric) as total_due
             , date(orderdate) as order_date
+            , coalesce(
+                case
+                    when status = 1 then 'In process'
+                    when status = 2 then 'Approved'
+                    when status = 3 then 'Backordered'
+                    when status = 4 then 'Rejected'
+                    when status = 5 then 'Shipped'
+                    when status = 6 then 'Cancelled'
+                end, 'not provided') as order_status
         from {{ source('sap_adw', 'salesorderheader') }}
     )
 select *
